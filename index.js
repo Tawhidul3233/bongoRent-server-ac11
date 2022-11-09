e7f67b8023895b4ba45affcf0fe44dbb1fcc0fd8
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-10 00:35:56 +0600

    mongodb added and 1 api create and data loded successfully

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +11,30 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kbyx5ha.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+     try{
+          const servicesCollection = client.db('bongorent').collection('services')
+
+          app.get('/services', async(req, res)=>{
+               const query = {}
+               const cursor = servicesCollection.find(query)
+               const services = await cursor.limit(3).toArray()
+               res.send(services)
+               console.log(services)
+          })
+
+     }
+     finally{
+
+     }
+}
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res)=>{
